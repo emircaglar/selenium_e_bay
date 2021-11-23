4e5b2324b6e62dcd0618c5b52c1278b6a3bafe82
emircaglar <emircaglar2906@gmail.com>
2021-11-23 20:54:42 +0100

    parent zweite seite

--- src/Parent_selenium.java
+++ src/Parent_selenium.java
@@ -1,17 +1,53 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Parent_selenium {
     public static  WebElement element;
 
-    public static   void von_get_von(String von_bis_Von, WebDriver driver, String value) {
-        element = driver.findElement(By.id(von_bis_Von));
+
+ public static void wait_element(WebElement element,WebDriver driver){
+
+     WebDriverWait wait=new WebDriverWait(driver,10 );
+     wait.until(ExpectedConditions.visibilityOf(element));
+
+ };
+ public static void wait_element_click(WebElement element,WebDriver driver){
+
+     WebDriverWait wait=new WebDriverWait(driver,10 );
+     wait.until(ExpectedConditions.elementToBeClickable(element));
+
+ };
+
+    public static   void von_get_von( WebDriver driver, String value) {
+
+        element = driver.findElement(By.id("srchrslt-brwse-price-min"));
+        wait_element(element,driver);
         element.sendKeys(value);
     };
 
-    public static   void von_get_bis(String von_bis_Bis, WebDriver driver, String value) {
-        element = driver.findElement(By.id(von_bis_Bis));
+    public static   void von_get_bis( WebDriver driver, String value) {
+        element = driver.findElement(By.id("srchrslt-brwse-price-max"));
+        wait_element(element,driver);
         element.sendKeys(value);
+    };
+    public static   void weiter_click( WebDriver driver) {
+        element = driver.findElement(By.xpath("//div[@class='browsebox-section-body']/div/button/i"));
+        wait_element_click(element,driver);
+        element.click();
+    };
+
+    public static   void options_click(WebElement element1 ,WebDriver driver) {
+        wait_element_click(element1,driver);
+        element1.click();
+    };
+
+
+    public static   void options_sendkeys(WebElement element1 ,WebDriver driver,String value) {
+        wait_element(element1,driver);
+        element1.sendKeys(value);
     };
 }
--- src/_03_e_bay_elektronik.java
+++ src/_03_e_bay_elektronik.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class _03_e_bay_elektronik {
-    public static void main(String[] args) {
+public class _03_e_bay_elektronik extends Parent_selenium {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "resourcces/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.ebay-kleinanzeigen.de/");
@@ -14,14 +14,33 @@ public class _03_e_bay_elektronik {
         WebElement akzeptieren = driver.findElement(By.xpath("//span[contains(@class,'jsx-4131758822')]"));
         akzeptieren.click();
 
-        WebElement elektronik_options=driver.findElement(By.xpath(" //ul[@id='home-ul-ctgrs']/li[10]/a"));
+        WebElement esc_click=driver.findElement(By.xpath("//a[@title='Close (Esc)']"));
+        esc_click.click();
+
+        WebElement elektronik_options=driver.findElement(By.xpath("//ul[@id='home-ul-ctgrs']/li[10]/a"));
         elektronik_options.click();
 
         WebElement zuerst_options=driver.findElement(By.id("sortingField-selector-inpt"));
-        zuerst_options.click();
+        Parent_selenium.options_click(zuerst_options,driver);
 
-        WebElement zuerst_günstige=driver.findElement(By.xpath("  //li[contains(@data-value,'PRICE_AMOUNT')] "));
-        zuerst_günstige.click();
+        WebElement zuerst_gunstige=driver.findElement(By.xpath("//li[contains(@data-value,'PRICE_AMOUNT')] "));
+        zuerst_gunstige.click();
+
+        WebElement von_get_von=driver.findElement(By.id("srchrslt-brwse-price-min"));
+        Parent_selenium.options_sendkeys(von_get_von,driver,"1000");
+        WebElement von_get_bis=driver.findElement(By.id("srchrslt-brwse-price-max"));
+        Parent_selenium.options_sendkeys(von_get_bis,driver,"2000");
+
+        Parent_selenium.weiter_click(driver);
+
+        WebElement zweite_seite=driver.findElement(By.xpath("//div[@class='pagination-pages']/a[1] "));
+        Parent_selenium.options_click(zweite_seite,driver);
+
+
+        Thread.sleep(2000);
+
+        driver.quit();
+
     }
 
 
